39b929b86b89ef472426dd405562a0201901319c
Mick <mick.mak@busicat.com>
2016-07-03 21:31:10 +0800

    Config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,17 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  # config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  # ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+     :tls => true,
+     :address => "smtp.gmail.com",
+     :port => 587,
+     :domain => "gmail.com",
+     :authentication => :plain,
+     :user_name => "mk.triniti@gmail.com",
+     :password => "k5085yrm"
+   }
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 1025 }
+  config.action_mailer.default_url_options = { host: 'localhost' }
 end
